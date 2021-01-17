7ed6252eb6a92294bbc0ff5a7ef0b3bd97aaf0e3
Yika <yika_w@outlook.com>
2021-01-17 17:58:59 +0800

    Create:1720.解码异或后的数组 https://leetcode-cn.com/problems/decode-xored-array/

---
+++ 1720.解码异或后的数组/main.go
@@ -0,0 +1,28 @@
+package main
+
+// 1720.解码异或后的数组
+// https://leetcode-cn.com/problems/decode-xored-array/
+func decode(encoded []int, first int) []int {
+
+}
+func test() {
+    type TestType struct{
+
+    }
+    ts := []TestType{
+        TestType{
+
+        },
+    }
+    for _,t:=range ts{
+        get :=
+        if !reflect.DeepEqual(get,t.want){
+            // 填充输出格式
+            fmt.Printf("%+v get:%v\n", t, get)
+        }
+    }
+    fmt.Println("Finished Test!")
+}
+func main() {
+    test()
+}
